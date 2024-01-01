294dcf0811ad166cbee58ea3d272f9edb28ee515
ngtsnn <tusocnau@gmail.com>
2024-01-01 22:48:10 +0700

    Feat: validate env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       cache: true,
       isGlobal: true,
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string()
+          .valid('development', 'production', 'test', 'provision')
+          .default('development'),
+        PORT: Joi.number().default(3000),
+        COOKIE: Joi.string().default(''),
+        PREFIX: Joi.string().default('api'),
+      }),
     }),
   ],
   controllers: [AppController],
